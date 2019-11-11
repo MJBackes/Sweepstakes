63e1b3dfd5bac4db5b6744b031aa0ad1f1a146c1
MJBackes <mjohnbackes@gmail.com>
2019-11-11 13:00:17 -0600

    Implemented the required methods in the Sweepstakes class.

--- Sweepstakes/Contestant.cs
+++ Sweepstakes/Contestant.cs
@@ -8,5 +8,19 @@ namespace Sweepstakes
 {
     public class Contestant
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public double RegistrationNumber { get; set; }
+        public Contestant()
+        {
+
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Registration Number: {RegistrationNumber}");
+        }
     }
 }
--- Sweepstakes/Sweepstakes.cs
+++ Sweepstakes/Sweepstakes.cs
@@ -8,5 +8,26 @@ namespace Sweepstakes
 {
     public class Sweepstakes
     {
+        public Dictionary<double, Contestant> Contestants;
+        public string Name { get; }
+        private Random rng;
+        public Sweepstakes(string name)
+        {
+            Name = name;
+            Contestants = new Dictionary<double, Contestant>();
+            rng = new Random();
+        }
+        public void RegisterContestant(Contestant contestant)
+        {
+            Contestants.Add(Contestants.Count, contestant);
+        }
+        public Contestant PickWinner()
+        {
+            return Contestants[rng.Next(Contestants.Count)];
+        }
+        public void PrintContestantInfo(Contestant contestant)
+        {
+            contestant.PrintInfo();
+        }
     }
 }
